06981f6f345af6be2430724b64935f717e605c66
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-09-16 20:01:34 +0200

    opdrachten

--- week-04-dom-events/eindopdracht/script.js
+++ week-04-dom-events/eindopdracht/script.js
@@ -2,3 +2,53 @@
 // taakToevoegen() — maak een <li> aan met een checkbox en verwijderknop
 // toonTaken() — werk de teller bij
 // Voeg listeners toe aan het formulier en de taken
+
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task-input');
+const taskList = document.querySelector('#tasks');
+const taskCount = document.querySelector('#counter');
+
+const taskToevoegen = (task) => {
+    const li = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Verwijder';
+
+    li.appendChild(checkbox);
+    li.appendChild(taskText);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+        toonTaken();
+    });
+};
+
+const toonTaken = () => {
+    const tasks = taskList.querySelectorAll('li');
+    taskCount.textContent = `Aantal taken: ${tasks.length}`;
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const task = taskInput.value.trim();
+
+  if (task === '') {
+    return;
+  }
+
+  taskToevoegen(task);
+  toonTaken();
+
+  taskInput.value = '';
+});
+
+toonTaken();
--- week-04-dom-events/oefening-2/script.js
+++ week-04-dom-events/oefening-2/script.js
@@ -1,2 +1,10 @@
 // Selecteer alle vakken met querySelectorAll als houvast
 // Loop met een for of loop door elk vak en voeg aan elk vak een click-event toe dat de klasse 'active' wisselt
+
+const boxes = document.querySelectorAll('.box');
+
+for (const box of boxes) {
+    box.addEventListener('click', () => {
+        box.classList.toggle('active');
+    });
+};
