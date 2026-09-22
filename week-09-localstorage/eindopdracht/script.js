3c6022ba07c2934aeeda5e276d42584574b2155d
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-09-22 11:34:43 +0200

    response

--- week-09-localstorage/eindopdracht/script.js
+++ week-09-localstorage/eindopdracht/script.js
@@ -60,7 +60,7 @@ const showItems = (items) => {
 document.querySelector('#loading').textContent = 'Laden...';
 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-  .then(Response => Response.json())
+  .then(response => response.json())
   .then(data => {
     document.querySelector('#loading').textContent = '';
 
